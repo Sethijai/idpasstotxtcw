872820f2b787b483649a704ffe230a8ae30891c3
Sethijai <148748128+Sethijai@users.noreply.github.com>
2024-09-21 12:18:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ api_id = 23713783
 
 api_hash = "2daa157943cb2d76d149c4de0b036a99"
 
-bot_token = "6811502626:AAEwThrPYxzIajowCUTeAaF9LqQQb2yMfHU"
+bot_token = "7194531764:AAEUwuO8Twfy_32NHMRGHxfAByNhjdTMESQ"
 
