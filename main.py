ba5b0fea9703a8f1d0a662d1a40d7017bbce74db
Sethijai <148748128+Sethijai@users.noreply.github.com>
2024-09-21 12:19:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ bot = Client(
     "CW",
     api_id= 23713783,
     api_hash= "2daa157943cb2d76d149c4de0b036a99",
-    bot_token= "6811502626:AAEwThrPYxzIajowCUTeAaF9LqQQb2yMfHU")
+    bot_token= "7194531764:AAEUwuO8Twfy_32NHMRGHxfAByNhjdTMESQ")
 
 logger = logging.getLogger()
 # thumb = os.environ.get("THUMB")
